8c830abec6d58d7cf615de548c8e01933fbabb71
spklpool <spklpool@gmail.com>
2022-07-24 14:35:48 -0400

    initial docker setup

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker stop f2lb-website
 docker rm f2lb-website
-docker build . -t node-f2lb-website
-docker run --name f2lb-website -p 3000:3000 -d node-f2lb-website
+docker build . -t f2lb-website-image
+docker run --name f2lb-website -p 3000:3000 -d f2lb-website-image
